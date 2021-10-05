87c288f7706f7240812263d25b6368d963837f35
Rajkumar <connect2rajkumar@gmail.com>
2021-10-05 19:13:41 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechnicalTest.Models;
+using TechnicalTest.API.Services;
+
+namespace TechnicalTestAPI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult<ProductDto> GetProducts()
+        //{
+        //    ProductRepository repo = new ProductRepository();
+        //    var prods = repo.GetProducts();
+        //    return prods;
+        //}
+    }
+}
